edc82e73dfd714e76e7edfe309558994e46b0ebb
JayMehtaJBM <69752445+JayMehtaJBM@users.noreply.github.com>
2020-09-16 22:23:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function cloak() {
+
+  var hours = $("#hour");
+  var minutes = $("#minutes");
+  var seconds = $("#seconds");
+
+  var h = new Date().getHours();
+  var m = new Date().getMinutes();
+  var s = new Date().getSeconds();
+
+  hours.text(h);
+  seconds.text(s);
+  minutes.text(m);
+
+}
+
+var setInterval = setInterval(cloak , 1000);
